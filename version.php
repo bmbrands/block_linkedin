d5d36124660844ec16ef8fd52bbf269aed21cbf4
BA CodeMonkey <bmbrands@gmail.com>
2012-09-28 12:33:19 +0200

    first commit

---
+++ version.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Bas Brands
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package moodle linkedIn block
+ *
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2012071600;
+$plugin->requires = 2011120500;   // Requires Moodle 2.2 or later
+$plugin->release = '1.1 (Build: 2012071600)';
+$plugin->maturity = MATURITY_STABLE;             // this version's maturity level
+$plugin->component = 'block_linkedin'; // Full name of the plugin (used for diagnostics)
